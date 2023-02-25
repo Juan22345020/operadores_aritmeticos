6d4c4d2f4eb5033c2152feed99e383157c306209
Juan22345020 <126175834+Juan22345020@users.noreply.github.com>
2023-02-25 09:53:00 -0500

    Add files via upload

---
+++ Quiz-25-feb.py
@@ -0,0 +1,20 @@
+# Ordenar un numero de 3 digitps
+
+print("----------------------------")
+print("-----NUMERO DE 3 DIGITOS----")
+print("----------------------------")
+
+
+# input
+
+n=int(input("Digite el valor: "))
+
+## Processing
+
+n=%100//10*1
+
+print("----------------------------")
+print("-----Resultado--------------")
+print("----------------------------")
+
+
