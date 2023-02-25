5ad0ba55c2338ec8aa355ce86722957cbea36192
Juan22345020 <126175834+Juan22345020@users.noreply.github.com>
2023-02-25 08:33:58 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # operadores_aritmeticos
 Programa para implentar operadores aritmeticos
+
+## DISEÑO
+
+### DIAGRAMA DE FLUJO
+
+[Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ operadores_aritmeticos.py
@@ -0,0 +1,35 @@
+# Programa para mplentar los operadores aritmeticos
+
+print("----------------------------------")
+print("-----OPERACIONES ARITMETICAS------")
+print("-----------------------------------")
+
+# input
+
+x=int(input("Digite el valor de x: "))
+y=int(input("Digite el valor de y: "))
+
+# procesing
+s= x + y
+r= x - y
+m= x * y
+d= x / y
+mod= x % y
+de= x // y
+p= x ** y
+
+
+print("--------------------------------------")
+print("---------RESULTADOS--------------")
+print("------------------------------------")
+
+
+
+
+print("Suma:  " + str(s))
+print("Resta: " + str(r))
+print("Multiplicacion: " + str(m))
+print("Division: " + str(d))
+print("Modulo: " + str(mod))
+print("Division entera: " + str(de))
+print("Potencia: " + str(p))
